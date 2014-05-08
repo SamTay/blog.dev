822417354c2dded3e084ffe831f9c53643d2f5f5
samtay <sam.chong.tay@gmail.com>
2014-05-08 17:33:10 -0400

    Added views_Template class from outside source

---
+++ application/views/template.php
@@ -0,0 +1,64 @@
+<?php
+
+class views_Template {
+
+/*
+ * @the registry
+ * @access private
+ */
+private $registry;
+
+/*
+ * @Variables array
+ * @access private
+ */
+private $vars = array();
+
+/**
+ *
+ * @constructor
+ * @access public
+ * @return void
+ *
+ */
+function __construct($registry) {
+    $this->registry = $registry;
+
+}
+
+
+ /**
+ * @set undefined vars
+ * @param string $index
+ * @param mixed $value
+ * @return void
+ */
+ public function __set($index, $value)
+ {
+    $this->vars[$index] = $value;
+ }
+
+
+function show($name) {
+    $path = ROOT . DS . 'application' . DS . 'views' . DS . $name . '.php';
+
+    if (file_exists($path) == false)
+    {
+        throw new Exception('Template not found in '. $path);
+        return false;
+    }
+
+    // Load variables
+    foreach ($this->vars as $key => $value)
+    {
+        $$key = $value;
+    }
+
+    include(ROOT . DS . 'templates' . 'header.php');
+    include ($path);
+    include(ROOT . DS . 'templates' . 'footer.php');
+}
+
+}
+
+?>
