5c80f13b7c8b6e5dc16396d1620755f417b8b1d5
samtay <sam.chong.tay@gmail.com>
2014-05-09 14:57:12 -0400

    Added abstract model class

---
+++ application/models/Model.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Not sure what to do with this model, but pretty sure the actual model classes
+ * will be derived from an abstract form, forcing certain properties/methods.
+ */
+abstract class Model {
+
+    /*
+     * Name of database table
+     *
+     * @var string
+     */
+    public $tableName = null;
+
+
+    /*
+     * The field name in the database
+     *
+     * @var string
+     */
+    public $primaryKey = null;
+
+
+    /*
+     * Container for data retrieved from database
+     *
+     * @var array
+     */
+    public $data = array();
+}
