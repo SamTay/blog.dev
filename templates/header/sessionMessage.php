918aa6d0cb41cb4835646d0c6631100a727a8961
samtay <sam.chong.tay@gmail.com>
2014-06-18 11:27:40 -0400

    Only include button for dangerous session messages

--- templates/header/sessionMessage.php
+++ templates/header/sessionMessage.php
@@ -4,7 +4,7 @@ echo '<div class="fixedElement col-md-offset-4 col-md-3">';
 echo '<div style="display: none" id="session-msg" class="alert alert-';
 echo !empty($sessionMsgTone) ? $sessionMsgTone : 'success';
 echo ' alert-dismissable">';
-if (!empty($sessionMsgTone)) {
+if ($sessionMsgTone == 'danger') {
 	echo '<button type="button" class="close" area-hidden="true">&times;</button>';
 }
 // If there is a message to user
